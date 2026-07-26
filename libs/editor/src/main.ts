5fcc02eea8abba124ba7d7e08ce0974f3d677875
Krzysztof Pająk <chris.araneo@gmail.com>
2026-07-26 22:48:05 +0200

    refactor: remove mountEditor funtion

--- libs/editor/src/main.ts
+++ libs/editor/src/main.ts
@@ -2,11 +2,20 @@ import './styles.css';
 
 import { match, P } from 'ts-pattern';
 
-import { mountEditor } from './editor';
-import { noop } from 'lodash-es';
+import { chain, noop } from 'lodash-es';
+import { bindPainting } from './editor/mount/bind-painting';
+import { createState } from './editor/state/create-state';
+import { createApp } from './editor/html/create-app';
+import { refreshState } from './editor/state/refresh-state';
 
 const { nullish } = P;
 
 match(document.querySelector<HTMLElement>('#app'))
   .with(nullish, noop)
-  .otherwise((app) => mountEditor(app));
+  .otherwise((app) =>
+    chain(createState())
+      .tap((state) => bindPainting(state))
+      .tap((state) => app.replaceChildren(createApp(state)))
+      .thru((state) => refreshState(state))
+      .value(),
+  );
