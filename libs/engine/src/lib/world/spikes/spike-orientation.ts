695a69ca9f67464ef74027976f0552666219d53e
Krzysztof Pająk <chris.araneo@gmail.com>
2026-07-27 22:47:31 +0200

    feat: add spikeOrientation function

--- libs/engine/src/lib/world/spikes/spike-orientation.ts
+++ libs/engine/src/lib/world/spikes/spike-orientation.ts
@@ -1,1 +1,21 @@
+import { match } from 'ts-pattern';
+
+import type { TileMap } from '../level/tile-map';
+import { TILE_SPIKE_CEILING } from './constants';
+
 export type SpikeOrientation = 'FLOOR' | 'CEILING';
+
+export const spikeOrientation = (
+  level: TileMap,
+  tileX: number,
+  tileY: number,
+): SpikeOrientation =>
+  match(
+    tileX >= 0 &&
+      tileX < level.width &&
+      tileY >= 0 &&
+      tileY < level.height &&
+      level.tiles[tileY][tileX] === TILE_SPIKE_CEILING,
+  )
+    .with(true, (): SpikeOrientation => 'CEILING')
+    .otherwise((): SpikeOrientation => 'FLOOR');
