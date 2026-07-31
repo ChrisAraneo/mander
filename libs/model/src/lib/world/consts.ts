73df481d666f5a1e162685b72a7d28fe7c79e270
Krzysztof Pająk <chris.araneo@gmail.com>
2026-07-31 18:38:31 +0200

    feat(model): update gravity and terminal velocity

--- libs/model/src/lib/world/consts.ts
+++ libs/model/src/lib/world/consts.ts
@@ -1,5 +1,5 @@
 export const MAX_JUMP_TILES = 5;
 
-export const GRAVITY = 1900;
-export const TERMINAL_VELOCITY = 1000;
+export const GRAVITY = 1850;
+export const TERMINAL_VELOCITY = 1100;
 export const MAX_TICK_SECONDS = 1 / 30;
