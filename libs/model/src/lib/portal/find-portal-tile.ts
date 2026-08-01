3673bd6549f852e8cb03e189c2023f56346685c0
Krzysztof Pająk <chris.araneo@gmail.com>
2026-08-01 11:11:19 +0200

    fix(model): update finding portal and find bottom tile

--- libs/model/src/lib/portal/find-portal-tile.ts
+++ libs/model/src/lib/portal/find-portal-tile.ts
@@ -1,7 +1,14 @@
 import type { Point } from '@mander/utils';
+import { match, P } from 'ts-pattern';
+
 import { findTile } from '../tile/find-tile';
 import type { Level } from '../level/level';
 import { TILE_PORTAL } from './portal';
+import { findPortalBottomTile } from './find-portal-bottom-tile';
+
+const { nullish } = P;
 
 export const findPortalTile = (level: Level): Point | null =>
-  findTile(level, TILE_PORTAL);
+  match(findTile(level, TILE_PORTAL))
+    .with(nullish, () => null)
+    .otherwise((top) => findPortalBottomTile(level, top));
