9fa707e877bc23d4800dc3153e7e9e515d3786d4
Krzysztof Pająk <chris.araneo@gmail.com>
2026-09-15 00:33:53 +0200

    refactor: delete files

--- libs/render/src/lib/tile/back-at.ts
+++
@@ -1,4 +0,0 @@
-import { backTileAt, isSolidTile, type Level } from '@mander/model';
-
-export const backAt = (level: Level, tileX: number, tileY: number): boolean =>
-  isSolidTile(backTileAt(level, tileX, tileY));
